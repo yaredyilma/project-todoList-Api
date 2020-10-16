4f0a414ec7c6abd6594e252d32e674fb78b5f57b
Yared <yayeyaye6@gmail.com>
2020-10-16 20:35:09 +0200

    updating init.js

--- public/src/init.js
+++ public/src/init.js
@@ -1,1 +1,15 @@
 'use strict';
+import { deleteHandler } from "./handler/delete-handler.js";
+import { postHandler } from "./handler/post-handler.js";
+import { updateHandler } from "./handler/update-handler.js";
+import { getItemsHandler } from "./handler/get-items-handler.js";
+
+document.getElementById('form-form')
+    .addEventListener('submit', postHandler);
+
+document.getElementById('row')
+    .addEventListener('click', deleteHandler);
+
+document.getElementById('row')
+    .addEventListener('click', updateHandler);
+window.addEventListener('DOMContentLoaded', getItemsHandler);
